0e0744a152a5bf83a8817d498b9d51ca093b7555
Filippo Tessarotto <zoeslam@gmail.com>
2021-10-31 12:55:21 +0100

    Continue

---
+++ test/SingleEncryptedZipArchiveAdapterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SlamFlysystemSingleEncryptedZipArchiveTest;
+
+use PHPUnit\Framework\TestCase;
+use SlamFlysystemSingleEncryptedZipArchive\SingleEncryptedZipArchiveAdapter;
+
+/**
+ * @covers \SlamFlysystemSingleEncryptedZipArchive\SingleEncryptedZipArchiveAdapter
+ *
+ * @internal
+ */
+final class SingleEncryptedZipArchiveAdapterTest extends TestCase
+{
+}
