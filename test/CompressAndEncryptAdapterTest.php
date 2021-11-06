24295c43a79423d30462b83ae255f3f51cfbf1ba
Filippo Tessarotto <zoeslam@gmail.com>
2021-11-06 20:52:52 +0100

    Tests in sub-namespace to make Psalm happy about internal class usage

--- test/CompressAndEncryptAdapterTest.php
+++ test/CompressAndEncryptAdapterTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SlamCompressAndEncryptProxyTest;
+namespace SlamCompressAndEncryptProxy\Test;
 
 use InvalidArgumentException;
 use League\Flysystem\AdapterTestUtilities\FilesystemAdapterTestCase;
--- test/EncryptorStreamFilterTest.php
+++ test/EncryptorStreamFilterTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace SlamCompressAndEncryptProxyTest;
+namespace SlamCompressAndEncryptProxy\Test;
 
 use PHPUnit\Framework\TestCase;
 use SlamCompressAndEncryptProxy\EncryptorStreamFilter;
